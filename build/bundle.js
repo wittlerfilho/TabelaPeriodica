be0af2b97532468122c34c86a47f7f40bf3fb04d
wittlerfilho <wittlerfilho@gmail.com>
2023-11-10 19:06:39 -0300

    mais coisas

--- build/bundle.js
+++ build/bundle.js
@@ -2528,7 +2528,7 @@ var app = function () {
                     i.innerHTML = '<svg class="dib pointer" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"></path><path fill="black" d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"></path></svg>',
                     n = h(),
                     t = y("div"),
-                    t.innerHTML = '<p class="svelte-1b8dv78">Tabela Periódica da Horta! 🥬</p>',
+                    t.innerHTML = '<p class="svelte-1b8dv78">Tabela Periódica dos Alimentos! 🥬</p>',
                     o = h(),
                     r = y("div"),
                     X(l.$$.fragment),
